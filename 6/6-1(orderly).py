10a7532fd6906ef4fbabf12e9887cf469a610645
jinatra <jinatra0816@gmail.com>
2021-12-02 16:06:07 +0900

    버블 정렬 로직 변경

--- 6/6-1(orderly).py
+++ 6/6-1(orderly).py
@@ -6,7 +6,7 @@ from typing import MutableSequence
 def bubble_sort(a: MutableSequence) -> None:
     n = len(a)
     for i in range(n-1):
-        for j in range(n-1):
+        for j in range(n-i-1):
             if a[j] > a[j+1]:
                 a[j], a[j+1] = a[j+1], a[j]
     return a
